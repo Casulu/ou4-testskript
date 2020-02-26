af7f134888b8c58ac4dd02cecfee5fcab3966516
Rasmus Lyxell <34223730+Casulu@users.noreply.github.com>
2020-02-26 17:09:20 +0100

    Add files via upload

--- parallel.sh
+++ parallel.sh
@@ -5,9 +5,8 @@ then
 	echo -e "Running script for testing $2 element values split into $3,  $4 times\n"
 	resultfolder=$1_$2_$3_$4
 	mkdir -p $resultfolder
-	printf "#!/bin/bash\n" > $resultfolder/runandsplit.sh
-	printf "$1, $2, $3, $4" > $resultfolder/input.txt
-    touch ./$resultfolder/raw.txt
+	printf "#!/bin/bash\n" >> $resultfolder/runandsplit.sh
+	printf "$1, $2, $3, $4" >> $resultfolder/input.txt
 	b=$(($3*$4))
 	for ((j=1;j<=$4;j++))
 	do
@@ -15,7 +14,7 @@ then
 		do
 			a=$(($2/$3))
 			a=$(($a*$i))
-			printf "echo $b tests left. Testing n=$a && ./$1 -n -t $a >> ./$resultfolder/raw.txt && " >> $resultfolder/runandsplit.sh
+			printf "echo $b tests left. Testing n=$a & ./$1 -n -t $a >> ./$resultfolder/raw.txt && " >> $resultfolder/runandsplit.sh
 			b=$(($b-1))
 		done
 	done
@@ -25,7 +24,7 @@ then
 	cp $SCRIPTPATH/plotter.m $resultfolder/
 	echo "Done!"
 else
-	echo -e "Usage: pathto/complete.sh {test executable location} {max elements} {element split amount} {runs per test}\n"
+	echo -e "Usage: ./complete.sh {test executable location} {max elements} {element split amount} {runs per test}\n"
 fi
 
 
