a2b4b03b641b2325068e72014fb037e743252133
Rasmus Lyxell <c19rll@cs.umu.se>
2021-02-22 13:21:55 +0100

    En plot-typ klar typ

--- pythonplotter/filerunner.py
+++ pythonplotter/filerunner.py
@@ -5,17 +5,34 @@ import numpy as np
 
 def splitTests(rawFile):
     loadedFile = np.loadtxt(rawFile, delimiter=",")
-    tests = []
+    tests = np.zeros((int(loadedFile.shape[0]/5), 2, 5), np.int32)
     for i in range(5):
-        tests.append(np.delete(loadedFile[np.nonzero(loadedFile[:, 0] == i+1)], 0, 1))
+        tests[:,:,i] = np.delete(loadedFile[np.nonzero(loadedFile[:, 0] == i+1)], 0, 1)
     return tests
 
-def plotTimeFunction(dataList):
-    i = 0
-    for data in dataList:
+def averageData(data):
+    elementAmounts = np.unique(data[:, 0, 0])
+    avg = np.zeros((elementAmounts.size, 2, 5), np.int32)
+    for i in range(5):
+        j = 0
+        for amount in elementAmounts:
+            avg[j, 1, i] = np.mean(data[data[:, 0, i] == amount, 1, i])
+            avg[j, 0] = amount
+            j = j + 1
+    return avg
+
+def plotTimeFunction(titles, dataList): 
+    for i in range(5):
         plt.figure(i)
-        i = i + 1
-        plt.plot(data[0], data[0])
+        plt.title(f"Test {i+1}")
+        for data in dataList:
+            plt.plot(data[:, 0, i], data[:, 1, i])
+        plt.legend(titles)
     plt.show()
 
-plotTimeFunction(splitTests(open('raw.txt', 'rb')))
+arr = splitTests(open('rawarray.txt', 'rb'))
+mtf = splitTests(open('rawmtf.txt', 'rb'))
+mtfAvg = averageData(mtf)
+arrAvg = averageData(arr)
+
+plotTimeFunction(["Mtf", "Array"],[mtfAvg, arrAvg])
